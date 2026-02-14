0c2d58236ab4a386a385d3a12f8245cad755bd63
Alexander <alexander.d.kirov@gmail.com>
2026-02-14 18:42:48 +0300

    fix parser

--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,6 @@
 import * as fs from 'fs'
 
-const getParseFile = (path) => {
+export default (path) => {
   const filePath = fs.readFileSync(path, {encoding: "utf-8"})
   return JSON.parse(filePath)
 }
-
-export default { getParseFile }
