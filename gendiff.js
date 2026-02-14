20d6247a47026d2ca9b94174f846bbebfe25cfb1
Alexander <alexander.d.kirov@gmail.com>
2026-02-14 14:56:45 +0300

    add option

--- gendiff.js
+++ gendiff.js
@@ -9,4 +9,5 @@ program
   .usage("[options]")
   .description("Compares two configuration files and shows a difference.")
   .version('0.0.1', '-V, --version')
+  .option('-f, --format [type]', 'output format')
   .parse()
