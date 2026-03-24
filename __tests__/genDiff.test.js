7bae55f55fffb798d4b183a81f17853c168e449e
Alexander <alexander.d.kirov@gmail.com>
2026-03-24 15:00:18 +0300

    corrected file path

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,7 +1,17 @@
 import { expect, test } from '@jest/globals'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import genDiff from '../src/index.js'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
+
 test('Сравнение плоских JSON-файлов', () => {
+  const filepath1 = getFixturePath('file1.json')
+  const filepath2 = getFixturePath('file2.json')
+
   const expected = `{
   - follow: false
     host: hexlet.io
@@ -11,5 +21,5 @@ test('Сравнение плоских JSON-файлов', () => {
   + verbose: true
 }`
 
-  expect(genDiff('file1.json', 'file2.json')).toBe(expected)
+  expect(genDiff(filepath1, filepath2)).toBe(expected)
 })
